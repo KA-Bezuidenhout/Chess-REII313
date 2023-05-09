d664049ee39bf696b35136b7ec73b121f8dd0819
KA-Bezuidenhout <karelbezhout@gmail.com>
2023-05-09 17:56:47 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@ int main(int argc, char *argv[])
 }
 
 
-//So Hierdie is wat ek doen eklke keer as ek iets verander:
+//So Hierdie is wat ek doen elke keer as ek iets verander:
 // 1. Verander iets.
 // 2. Gaan onder Tools / Git / Local Respitory / Commit
-// 3. Sê "Save All"
-// 4. Gaan na Tools / Git / Remote Respitoy / Push
-// 3. Check "Select All" aan die regter kant en
+// 3. Check "Select All" aan die regter kant en Commit Regs Onder In hoekerig
+// 4. Gaan na Tools / Git / Remote Respitory / Push
+
+
+//Wat ek dink jy moet doen om dit ook so te doen:
+// Gaan na die link en Download daardie files waarookal jy wil.
 //
+//
+
+
+//Om goed te kry van ander persoon se veranderine af:
+// Tools / Git / Remote Respitory / Fetch
