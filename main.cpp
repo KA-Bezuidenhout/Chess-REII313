393ccda30c0839359f46fda337ab7758cbb0a855
KA-Bezuidenhout <karelbezhout@gmail.com>
2023-05-09 17:52:50 +0200

    Comments V1

--- main.cpp
+++ main.cpp
@@ -8,4 +8,10 @@ int main(int argc, char *argv[])
 }
 
 
-//Kan jy Hieride Lees?
+//So Hierdie is wat ek doen eklke keer as ek iets verander:
+// 1. Verander iets.
+// 2. Gaan onder Tools / Git / Local Respitory / Commit
+// 3. Sê "Save All"
+// 4. Gaan na Tools / Git / Remote Respitoy / Push
+// 3. Check "Select All" aan die regter kant en
+//
