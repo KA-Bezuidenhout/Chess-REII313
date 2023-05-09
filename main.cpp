9d38c4c302b97f4ff4e84c33fe6cc55d1b533e20
KA-Bezuidenhout <karelbezhout@gmail.com>
2023-05-09 17:59:10 +0200

    comment

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 // 1. Verander iets.
 // 2. Gaan onder Tools / Git / Local Respitory / Commit
 // 3. Sê "Save All"
-// 4. Check "Select All" aan die regter kant en Commit Regs Onder In hoekerig
-// 5. Gaan na Tools / Git / Remote Respitory / Push
+// 4. Check "Select All" aan die regter kant en  Gee 'n comment aan die linkerkant wat ek verander het, dan Commit Regs Onder In hoekerig
+// 5. Gaan na Tools / Git / Remote Respitory / Push (Dan upload ek dit na die link)
 
 
 //Wat ek dink jy moet doen om dit ook so te doen:
